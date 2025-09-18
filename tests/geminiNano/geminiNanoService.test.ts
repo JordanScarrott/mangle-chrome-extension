1a71fbf06e7d189fc395d8ed015df0a7f985c934
Jordan Scarrott <jordan.scarrott@vastech.co.za>
2025-09-18 16:08:38 +0200

    Changed test name

--- tests/geminiNano/geminiNanoService.test.ts
+++ tests/geminiNano/geminiNanoService.test.ts
@@ -3,7 +3,7 @@ import { exampleText } from "tests/geminiNano/exampleData";
 import { describe, test } from "vitest";
 
 describe("geminiNanoService", () => {
-    test("v1 prompt", async () => {
+    test("v2 prompt", async () => {
         const response = await usePrompt().prompt(exampleText(2));
     });
 });
